eb9a086306df95ed200e44b04464f81b73196904
juanitanassarlondono-cyber <juanitanassarlondono@gmail.com>
2026-02-26 10:28:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ st.writer("Facilmente puedo realizar backend y frontend.")
 image = Image.open('caballitos.jpg')
 st.image(image, caption='Libertad')
 
-texto = st.text_input('escribe algo´,'este es el texto' )
+texto = st.text_input("escribe algo","este es el texto")
 st. write('el texto escrito es' , texto)
                     
