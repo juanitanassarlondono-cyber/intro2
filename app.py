86be90516167b0cc72980ea732a81345d12a2bba
juanitanassarlondono-cyber <juanitanassarlondono@gmail.com>
2026-02-26 10:13:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import streamlit as st
+from PIL import Image
 
-st.title("Mi primera app")
+st.title("Bienvenido a primera app")
+
+st.header("Aqui empiezas a navegar en mi app")
+st.writer("Facilmente puedo realizar backend y frontend.")
+image = Image.open('Interfaces Multi2.png
