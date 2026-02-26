cd3d06fe021bd6ab3ba395b26f3f24a301bf5917
juanitanassarlondono-cyber <juanitanassarlondono@gmail.com>
2026-02-26 10:28:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title("Bienvenido a primera app")
 
 st.header("Aqui empiezas a navegar en mi app")
 st.writer("Facilmente puedo realizar backend y frontend.")
-image = Image.open('caballitos.jpg')
+image = Image.open("caballitos.jpg")
 st.image(image, caption='Libertad')
 
 texto = st.text_input("escribe algo","este es el texto")
