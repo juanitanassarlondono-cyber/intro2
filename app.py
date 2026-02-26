028acc41bec3dd65f22bdf2fc23da08052c8a2fc
juanitanassarlondono-cyber <juanitanassarlondono@gmail.com>
2026-02-26 10:49:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,4 +10,30 @@ st.image(image, caption='Libertad')
 
 texto = st.text_input("escribe algo","este es el texto")
 st. write('el texto escrito es' , texto)
+
+
+st.subheader("ahora son 3 columnas")
+
+col1, col2 = st.columna(2)
+with col1:
+  st.subheader("Esta es la primera columna")
+  st.subheader("Esta es la primera columna")
+  st.write("Las interfaces multimodalesmejoran la experiencia de usuario")
+  resp = st.checkbox("Estoy de acuerdo")
+  if resp:
+   st.write("Correcto!")
+
+with col2:
+  st.subheader("Esta es la segunda columna")
+  modo == st.radio("Que modalidad es la primera en tu interfaz", ("Visual", "auditiva", "táctil"))
+  if modo == "Visual":
+    st.write("La vista es fundamentalpara tu interfaz")
+  if modo == "auditiva":
+    st.write("La audición es fundamental para tu interfaz")
+  if modo == "Táctil):
+   st.write("El tacto es fundamental para tu interfaz")
+
+st.subheader("Uso de Botones")
+if st.button("Presiona el botón"):
+  
                     
